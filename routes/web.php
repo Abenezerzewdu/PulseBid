cfa4dfce371c7fef8c41cf0846a52f0c73b84ccf
abenezer <abenezerzewdu11@gmail.com>
2026-04-09 05:04:34 +0300

    cleaned the bid

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuctionController;
+use App\Http\Controllers\BidController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -20,13 +21,18 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+//profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//auction
+    Route::get('/auctions/create', [AuctionController::class, 'create']);
+    Route::post('/auctions', [AuctionController::class, 'store']);
+    Route::get('/auctions/{auction}', [AuctionController::class, 'show']) ->name('auctions.show');
+    Route::post('/auctions/{auction}/bid', [BidController::class, 'store'])
+    ->name('auctions.bid');
 });
 
-Route::get('/auctions/create', [AuctionController::class, 'create']);
-Route::post('/auctions', [AuctionController::class, 'store']);
-Route::get('/auctions/{auction}', [AuctionController::class, 'show']) ->name('auctions.show');
+
  
 require __DIR__.'/auth.php';
